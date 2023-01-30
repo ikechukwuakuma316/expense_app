90786f8e5aee8e9bb04187b1a41ba76c5285b401
ikechukwuakuma316 <ikechukwuakuma316@gmail.com>
2023-01-30 16:41:10 +0100

    box decoration

--- lib/main.dart
+++ lib/main.dart
@@ -57,6 +57,13 @@ class MyHomePage extends StatelessWidget {
                         vertical: 10,
                         horizontal: 15,
                       ),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Colors.black,
+                          width: 2,
+                        ),
+                      ),
+                      padding: EdgeInsets.all(10),
                       child: Text(
                         tx.amount.toString(),
                       ),
